bdf98a72e68432a5ea6aadd2fb1e7be77db10c40
vinurockz <vineethes800@gmail.com>
2021-09-07 19:07:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ttfmt2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ttfmt2)
---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, VERSION } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+class investigation{
+  id: number;
+  invetigationType: string;
+  InvestigationId: number;
+  rate: number;
+  isSelected: boolean;
+  testName:string;
+  isAdded: boolean;
+}
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor() {}
+  ngOnInit() {
+  }
+
+}
