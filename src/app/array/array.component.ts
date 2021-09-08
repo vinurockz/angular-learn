37ba34b60dcd0cde66aac57f788a06e50810eec1
vinurockz <vineethes800@gmail.com>
2021-09-08 09:41:35 +0530

    2 files modified array filter compleate

--- src/app/array/array.component.ts
+++ src/app/array/array.component.ts
@@ -9,28 +9,29 @@ export class ArrayComponent implements OnInit {
   carDetails: any;
   nameDetails: any;
   regDetails: any;
-  filterDetails: any;
-  constructor() { }
-  students= [{ "name":"John", "reg":11, "car":null },{ "name":"vinu", "reg":12, "car":true },{ "name":"manu", "reg":13, "car":false },
-  { "name":"binu", "reg":14, "car":true },{ "name":"raju", "reg":15, "car":false },{ "name":"sonu", "reg":16, "car":true }];
+  lth1m: any;
+  filterDetails: any[] = [];
+  constructor() {}
+  students = [
+    { name: 'John', reg: 11, car: null },
+    { name: 'vinu', reg: 12, car: true },
+    { name: 'manu', reg: 13, car: false },
+    { name: 'binu', reg: 14, car: true },
+    { name: 'raju', reg: 15, car: false },
+    { name: 'sonu', reg: 16, car: true }
+  ];
   ngOnInit() {
-   
-   this.allArray();
+    this.allArray();
   }
 
-  allArray(){
-    this.carDetails=this.students.map(dat=> dat.car);
-    this.nameDetails=this.students.map(dat=> dat.name);
-    this.regDetails=this.students.map(dat=> dat.reg);
-  const fil=this.students.filter(dat=>dat.reg < 14);
-    for( let i; i<this.filterDetails; i++){
-      console.log(this.filterDetails['name'])
+  allArray() {
+    this.carDetails = this.students.map(dat => dat.car);
+    this.nameDetails = this.students.map(dat => dat.name);
+    this.regDetails = this.students.map(dat => dat.reg);
+    const fil = this.students.filter(dat => dat.reg < 14);
+    for (let i = 0; i < fil.length; i++) {
+      this.lth1m = fil.map(dat=>dat.name);
+      console.log(this.lth1m);
     }
-   this.filterDetails= this.filterDetails['name']
-
-  console.log(fil);
-  
   }
-
-
 }
