649ec4aea702260666d86edfbff304d3eed7cc4c
vinurockz <vineethes800@gmail.com>
2021-09-09 08:49:51 +0530

    1 file added a const file saved

---
+++ src/app/cost-files/order-data.ts
@@ -0,0 +1,54 @@
+export const order_data = [
+  {
+    id: 0,
+    customerID: 14,
+    customerName: 'Zachary Kipping',
+    itemCount: 3
+  }, {
+    id: 1,
+    customerID: 7,
+    customerName: 'Cory Rylan',
+    itemCount: 8
+  }, {
+    id: 2,
+    customerID: 16,
+    customerName: 'Jeff Bezos',
+    itemCount: 5
+  }, {
+    id: 3,
+    customerID: 19,
+    customerName: 'Zach Kipling',
+    itemCount: 6
+  }, {
+    id: 4,
+    customerID: 4,
+    customerName: 'Bill Nye',
+    itemCount: 2
+  }, {
+    id: 5,
+    customerID: 21,
+    customerName: 'Sundar Pichai',
+    itemCount: 17
+  }, {
+    id: 6,
+    customerID: 12,
+    customerName: 'Tim Cook',
+    itemCount: 9
+  }, {
+    id: 6,
+    customerID: 11,
+    customerName: 'Jack Balbes',
+    itemCount: 4
+  },
+    {
+    id: 7,
+    customerID: 15,
+    customerName: 'John Baur',
+    itemCount: 6
+  }, {
+    id: 8,
+    customerID: 23,
+    customerName: 'Bill Odom',
+    itemCount: 14
+  },
+];
